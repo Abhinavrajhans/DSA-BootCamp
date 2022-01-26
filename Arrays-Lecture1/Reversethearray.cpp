f72ae2258f15cb7c5439fd88b95307696747e84a
ABHINAV RAJHANS <68992789+Abhinavrajhans@users.noreply.github.com>
2022-01-26 14:06:02 +0530

    Update Reversethearray.cpp

--- Arrays-Lecture1/Reversethearray.cpp
+++ Arrays-Lecture1/Reversethearray.cpp
@@ -1,1 +1,16 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int len;
+  cin>>len;
+  while(len--) {
+     int n;
+     cin>>n;
+     long long int a[n],b[n];
+     for(int i=n-1;i>=0;i--) cin>>a[i];
+     for(int i=0;i<n;i++)cout<<a[i]<<" ";
+     cout<<endl;
+  }
+}
