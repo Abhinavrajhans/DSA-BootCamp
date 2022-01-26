7f7a96fbb1ed7969e21be85c3274f7c8b9586933
ABHINAV RAJHANS <68992789+Abhinavrajhans@users.noreply.github.com>
2022-01-26 14:10:14 +0530

    Update Reversethearray.cpp

--- Arrays-Lecture1/Reversethearray.cpp
+++ Arrays-Lecture1/Reversethearray.cpp
@@ -3,14 +3,20 @@
 using namespace std;
 int main()
 {
-  int len;
-  cin>>len;
-  while(len--) {
+  int t;
+  cin>>t;                                                 //t is for Test-Cases
+  while(t--) {
      int n;
-     cin>>n;
-     long long int a[n],b[n];
-     for(int i=n-1;i>=0;i--) cin>>a[i];
-     for(int i=0;i<n;i++)cout<<a[i]<<" ";
+     cin>>n;                                              //taking n the size of the array as input.
+     long long int a[n];                                 //making a array which takes values of type long long int.
+     for(int i=n-1;i>=0;i--) cin>>a[i];                  //Main logic written below.
+     for(int i=0;i<n;i++)cout<<a[i]<<" ";                //printing the array
      cout<<endl;
   }
 }
+
+/*
+Main Logic : the Logic we are using is we are placing the ith element of the array at position n-1-i or in simple words 
+we are placing the elements from behind . and printing the element from front.
+*/
+
