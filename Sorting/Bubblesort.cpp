fa48881c43705c835948079a491cae6aace8d846
ABHINAV RAJHANS <68992789+Abhinavrajhans@users.noreply.github.com>
2022-02-26 14:54:44 +0530

    Update Bubblesort.cpp

--- Sorting/Bubblesort.cpp
+++ Sorting/Bubblesort.cpp
@@ -8,7 +8,7 @@ void swap(int i, int j, int arr[])
 }
 void Bubblesort(int arr[], int n)
 {
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n - 1; i++)
 	{
 		for (int j = 0; j < n - i - 1 ; j++)
 		{
