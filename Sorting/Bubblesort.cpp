1b5cb87f6adbf1f5965a88582f233e53b1130cf5
ABHINAV RAJHANS <68992789+Abhinavrajhans@users.noreply.github.com>
2022-02-26 14:56:23 +0530

    Update Bubblesort.cpp

--- Sorting/Bubblesort.cpp
+++ Sorting/Bubblesort.cpp
@@ -10,11 +10,14 @@ void Bubblesort(int arr[], int n)
 {
 	for (int i = 0; i < n - 1; i++)
 	{
+		bool is_swap = false;
 		for (int j = 0; j < n - i - 1 ; j++)
 		{
 			if (arr[j] <= arr[j + 1])continue;
 			swap(j, j + 1, arr);
+			is_swap = true;
 		}
+		if (is_swap == false)break;
 	}
 }
 int main()
